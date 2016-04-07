2c6a212ccba38b446fe74e839d661e7824fb486f
Dragos Tiselice <dragostiselice@gmail.com>
2016-04-07 09:51:07 +0300

    Updated mrusty documentation.

--- implementors/mrusty/trait.MRubyImpl.js
+++ implementors/mrusty/trait.MRubyImpl.js
@@ -1,5 +1,5 @@
 (function() {var implementors = {};
-implementors['mrusty'] = [];implementors['mrusty'] = [];implementors['mrusty'] = [];
+implementors['mrusty'] = [];implementors['mrusty'] = [];implementors['mrusty'] = [];implementors['mrusty'] = [];
 
             if (window.register_implementors) {
                 window.register_implementors(implementors);
