2f27f15776af0c3a9efc522d58a2b0d5478dd59d
Dragos Tiselice <dragostiselice@gmail.com>
2016-04-20 14:28:12 +0300

    Updated mrusty documentation.

--- implementors/core/ops/trait.Neg.js
+++ implementors/core/ops/trait.Neg.js
@@ -1,5 +1,5 @@
 (function() {var implementors = {};
-implementors['libc'] = [];implementors['time'] = ["impl <a class='trait' href='https://doc.rust-lang.org/nightly/core/ops/trait.Neg.html' title='core::ops::Neg'>Neg</a> for <a class='struct' href='time/struct.Duration.html' title='time::Duration'>Duration</a>",];implementors['anima_engine'] = ["impl <a class='trait' href='https://doc.rust-lang.org/nightly/core/ops/trait.Neg.html' title='core::ops::Neg'>Neg</a> for <a class='struct' href='anima_engine/math/struct.Vector.html' title='anima_engine::math::Vector'>Vector</a>",];implementors['anima_engine'] = ["impl <a class='trait' href='https://doc.rust-lang.org/nightly/core/ops/trait.Neg.html' title='core::ops::Neg'>Neg</a> for <a class='struct' href='anima_engine/math/struct.Vector.html' title='anima_engine::math::Vector'>Vector</a>",];
+implementors['libc'] = [];implementors['time'] = ["impl <a class='trait' href='https://doc.rust-lang.org/nightly/core/ops/trait.Neg.html' title='core::ops::Neg'>Neg</a> for <a class='struct' href='time/struct.Duration.html' title='time::Duration'>Duration</a>",];
 
             if (window.register_implementors) {
                 window.register_implementors(implementors);
